822bd34be48bdae07dcc0fca6dec21e560d6e5a5
feichay10 <alu0101364544@ull.edu.es>
2022-03-07 10:16:56 +0000

    Primer commit

--- cd.v
+++ cd.v
@@ -3,12 +3,12 @@ module cd(input wire clk, reset, s_inc, s_inm, we3, wez, input wire [2:0] op_alu
 wire [15:0] instruccion;
 wire [9:0] Mux_EntPC, SalPC, sum_mux, Mux_EntBR;
 wire [7:0] R1, R2, Sal_alu, Mux_Sal_toBR;
-wire alu_ffz
+wire alu_ffz;
 
 assign Uno_10bits = 10'b0000000001;
 
-// Program Counter: como lo hago?
-
+// Program Counter: como lo hago? #(parameter WIDTH = 8)????
+registro #() PC (clk, reset, Mux_EntPC, SalPC);
 
 // Memoria de Programa
 memprog memoria(clk, SalPC, instruccion);
@@ -17,18 +17,20 @@ memprog memoria(clk, SalPC, instruccion);
 sum sumador_A(SalPC, Uno_10bits, sum_mux);
 
 // Multiplexor A 
-mux2 multiplexor_A(instruccion[9:0], sum_mux, s_inc, Mux_EntPC);
+mux2 #() multiplexor_A (instruccion[9:0], sum_mux, s_inc, Mux_EntPC);
 
 // Banco de registros
 regfile banco_registros(clk, we3, instruccion[11:8], instruccion[7:4], instruccion[3:0], Mux_EntBR, R1, R2);
 
-//ALU
+// ALU
 alu alu_cpu(R1, R2, op_alu, Sal_alu, alu_ffz);
 
 // Multiplexor B 
-mux2 multiplexor_B(Sal_alu, instruccion[11:4], Mux_Sal_toBR);
+mux2 #() multiplexor_B (Sal_alu, instruccion[11:4], Mux_Sal_toBR);
 
 // Flip-Flop Z
 ffd ffz(clk, reset, alu_ffz, wez, z);
+
+assign opcode = instruccion[15:10];
 
 endmodule
