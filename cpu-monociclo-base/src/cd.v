86cf513d4597a101907fc515dfc3800a6adf8799
feichay10 <alu0101364544@ull.edu.es>
2023-02-15 10:57:43 +0000

    Cambio de nombre de las señales

--- cpu-monociclo-base/src/cd.v
+++ cpu-monociclo-base/src/cd.v
@@ -21,33 +21,33 @@
 module cd(input wire clk, reset, s_inc, s_inm, we3, wez, input wire [2:0] op_alu, output wire z, output wire [5:0] opcode);
 //Camino de datos de instrucciones de un solo ciclo
 wire [15:0] instruccion;
-wire [9:0] Mux_EntPC, SalPC, sum_mux, Mux_EntBR;
-wire [7:0] R1, R2, Sal_alu, Mux_Sal_toBR;
+wire [9:0] SalMuxInc_EntPC, SalPC, SalSum_EntMuxInc, Mux_EntBR;
+wire [7:0] Reg1, Reg2, SalALU, SalMuxInm_EntBR;
 wire alu_ffz;
 
 // Program Counter: 
-registro #(10) PC (clk, reset, Mux_EntPC, SalPC);
+registro #(10) PC (clk, reset, SalMuxInc_EntPC, SalPC);
 
 // Memoria de Programa
-memprog memoria(clk, SalPC, instruccion);
+memprog Memoria (clk, SalPC, instruccion);
 
 // Sumador A 
-sum sumador_A(SalPC, 10'b0000000001, sum_mux);
+sum Sumador(SalPC, 10'b0000000001, SalSum_EntMuxInc);
 
-// Multiplexor A 
-mux2 #(10) multiplexor_A (instruccion[9:0], sum_mux, s_inc, Mux_EntPC);
+// Multiplexor Inc
+mux2 #(10) MuxInc(instruccion[9:0], SalSum_EntMuxInc, s_inc, SalMuxInc_EntPC);
 
 // Banco de registros
-regfile banco_registros(clk, we3, instruccion[11:8], instruccion[7:4], instruccion[3:0], Mux_Sal_toBR, R1, R2);
+regfile BancoReg(clk, we3, instruccion[11:8], instruccion[7:4], instruccion[3:0], SalMuxInm_EntBR, Reg1, Reg2);
 
 // ALU
-alu alu_cpu(R1, R2, op_alu, Sal_alu, alu_ffz);
+alu ALU (Reg1, Reg2, op_alu, SalALU, alu_ffz);
 
-// Multiplexor B 
-mux2 #(8) multiplexor_B (Sal_alu, instruccion[11:4], s_inm, Mux_Sal_toBR);
+// Multiplexor Inm
+mux2 #(8) MuxInm (SalALU, instruccion[11:4], s_inm, SalMuxInm_EntBR);
 
 // Flip-Flop Z
-ffd ffz(clk, reset, alu_ffz, wez, z);
+ffd FFZ (clk, reset, alu_ffz, wez, z);
 
 assign opcode = instruccion[15:10];
 
