688e16d6b8ba3193f7f08860d29183c0d925f940
feichay10 <alu0101364544@ull.edu.es>
2023-02-22 09:47:20 +0000

    Actualizamos el testbench

--- cpu-monociclo-base/src/cpu.v
+++ cpu-monociclo-base/src/cpu.v
@@ -19,15 +19,14 @@
  */
 
 module cpu(input wire clk, reset);
-//Procesador sin memoria de datos de un solo ciclo
-wire s_inc, s_inm, we3, wez, z;
-wire [2:0] op_alu;
-wire [5:0] opcode;
+  //Procesador sin memoria de datos de un solo ciclo
+  wire s_inc, s_inm, we3, wez, z;
+  wire [2:0] op_alu;
+  wire [5:0] opcode;
 
-// Camino de datos
-cd caminodatos(clk, reset, s_inc, s_inm, we3, wez, op_alu, z, opcode);
+  // Camino de datos
+  cd caminodatos(clk, reset, s_inc, s_inm, we3, wez, op_alu, z, opcode);
 
-// Unidad de Control
-uc unidadcontrol(opcode, z, s_inc, s_inm, we3, wez, op_alu);
-
+  // Unidad de Control
+  uc unidadcontrol(opcode, z, s_inc, s_inm, we3, wez, op_alu);
 endmodule
--- cpu-monociclo-base/src/cpu_tb.v
+++ cpu-monociclo-base/src/cpu_tb.v
@@ -1,23 +1,3 @@
-/**
- *
- * Universidad de La Laguna
- * Escuela Superior de Ingeniería y Tecnología
- * Grado en Ingeniería Informática
- * Asignatura: Diseño de Procesadores
- * Curso: 3º
- * Proyecto 1: Diseño de la unidad de control de una CPU simple de un solo ciclo
- * @file cpu_tb.v
- * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
- * @brief 
- * @version 0.1
- * @date 01-02-2023
- * @compilation: iverilog -o cpu_base cpu_tb.v alu.v cd.v componentes.v cpu.v memprog.v uc.v
- * @simulation: vvp cpu_base
- *
- * @copyright Copyright (c) 2023
- *
- */
-
 `timescale 1 ns / 10 ps
 
 module cpu_tb;
@@ -40,22 +20,14 @@ initial
 begin
   $dumpfile("cpu_tb.vcd");
   $dumpvars;
-  $dumpvars(0,cpu_tb.micpu.camino_datos.banco_registros.regb[1]);
-  $dumpvars(0,cpu_tb.micpu.camino_datos.banco_registros.regb[2]);
-  $dumpvars(0,cpu_tb.micpu.camino_datos.banco_registros.regb[3]);
   reset = 1;  //a partir del flanco de subida del reset empieza el funcionamiento normal
   #10;
   reset = 0;  //bajamos el reset 
 end
 
 initial
-
 begin
-  #(50*60);  //Esperamos 9 ciclos o 9 instrucciones
-  $write ("R1 =%d\nR2 =%d =%d\n",
-  cpu_tb.micpu.camino_datos.banco_registros.regb[1],
-  cpu_tb.micpu.camino_datos.banco_registros.regb[2],
-  cpu_tb.micpu.camino_datos.banco_registros.regb[3]);
+  #(9*60);  //Esperamos 9 ciclos o 9 instrucciones
   $finish;
 end
 
