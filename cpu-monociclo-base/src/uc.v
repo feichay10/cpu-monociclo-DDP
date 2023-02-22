1ef69ce4eab8889cc4a0fa0cd5c4de83ed4abc2f
feichay10 <alu0101364544@ull.edu.es>
2023-02-22 10:16:30 +0000

    Mejoramos el script

--- cpu-monociclo-base/src/uc.v
+++ cpu-monociclo-base/src/uc.v
@@ -25,128 +25,128 @@ always @(*)
   //Salto incondicional
   6'b001000:
     begin
-      s_inc = 1'b0;
-      s_inm = 1'b0;
-      we3 = 1'b0;
-      wez = 1'b0;
-      op_alu = 3'b000;
+      s_inc=1'b0;
+      s_inm=1'b0;
+      we3=1'b0;
+      wez=1'b0;
+      op_alu=3'b000;
     end
   
   //Salto Condicional JZ
   6'b001001:
     begin
-      if (z == 1'b1)
-        s_inc = 1'b0;
+      if (z == 1'b1) 
+        s_inc=1'b0;
       else 
-        s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b0;
-      wez = 1'b0;
-      op_alu = 3'b000;
+        s_inc=1'b1;
+      s_inm=1'b0;
+      we3=1'b0;
+      wez=1'b0;
+      op_alu=3'b000;
     end
 
   //Salto Condicional JNZ
   6'b001010:
     begin
-      if (z == 1'b1)
-        s_inc = 1'b1;
+      if (z == 1'b1) 
+        s_inc=1'b1;
       else 
-        s_inc = 1'b0;
-      s_inm = 1'b0;
-      we3 = 1'b0;
-      wez = 1'b0;
-      op_alu = 3'b000;
+        s_inc=1'b0;
+      s_inm=1'b0;
+      we3=1'b0;
+      wez=1'b0;
+      op_alu=3'b000;
     end
 
   //Carga Inmediata
   6'b0000??:
     begin
-      s_inc = 1'b1;
-      s_inm = 1'b1;
-      we3 = 1'b1;
-      wez = 1'b0;
-      op_alu = 3'b000;
+      s_inc=1'b1;
+      s_inm=1'b1;
+      we3=1'b1;
+      wez=1'b0;
+      op_alu=3'b000;
     end
 
   //ALU (Oper. A)
   6'b1000??:
     begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b000;
+      s_inc=1'b1;
+      s_inm=1'b0;
+      we3=1'b1;
+      wez=1'b1;
+      op_alu=3'b000;
     end
   
   //ALU (Oper. A negado)
   6'b1001??:
-    begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b001;
-    end
+  begin
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b001;
+  end
 
   //ALU (Oper. A + B)
   6'b1010??:
   begin
-    s_inc = 1'b1;
-    s_inm = 1'b0;
-    we3 = 1'b1;
-    wez = 1'b1;
-    op_alu = 3'b010;
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b010;
   end
 
   //ALU (Oper. A - B)
   6'b1011??:
-    begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b011;
-    end
+  begin
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b011;
+  end
 
   //ALU (Oper. A AND B)
   6'b1100??:
-    begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b100;
-    end
+  begin
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b100;
+  end
 
   //ALU (Oper. A OR B)
   6'b1101??:
-    begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b101;
-    end
+  begin
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b101;
+  end
 
   //ALU (Oper. -A)
-  6'b1110??: 
-    begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b110;
-    end
+  6'b1110??:
+  begin
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b110;
+  end
 
   //ALU (Oper. -B)  
-  6'b1111??: 
-    begin
-      s_inc = 1'b1;
-      s_inm = 1'b0;
-      we3 = 1'b1;
-      wez = 1'b1;
-      op_alu = 3'b111;
-    end
+  6'b1111??:
+  begin
+    s_inc=1'b1;
+    s_inm=1'b0;
+    we3=1'b1;
+    wez=1'b1;
+    op_alu=3'b111;
+  end
 
   default: 
     begin
